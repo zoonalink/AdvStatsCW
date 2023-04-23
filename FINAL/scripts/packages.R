044e2629bd8a9b607db3c8f7ddaf5423563da595
petter Rstudio <zoonalink@gmail.com>
2023-04-23 22:44:46 +0100

    weekend work - upto eda

---
+++ FINAL/scripts/packages.R
@@ -0,0 +1,15 @@
+
+cran_repo <- "https://www.stats.bris.ac.uk/R/"
+# vector of packages to install/load
+packages <- c("dplyr","citr", "readr", "naniar", "gt", "gtsummary", "patchwork", "ggplot2", "ggfortify", "knitr", "gtExtras", "caret")
+
+for (package in packages) {
+  if (!require(package, character.only = TRUE)) {
+    if (package == "citr") {
+      devtools::install_github("crsh/citr")
+    } else {
+      install.packages(package, repos = cran_repo)
+    }
+    library(package, character.only = TRUE)
+  }
+}
