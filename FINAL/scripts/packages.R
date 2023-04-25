3293acdd5f5d5d2db977ce000d016bc3e1776019
zoonalink <zoonalink@gmail.com>
2023-04-25 23:49:30 +0100

    02 data update - to split

--- FINAL/scripts/packages.R
+++ FINAL/scripts/packages.R
@@ -1,7 +1,7 @@
 
 cran_repo <- "https://www.stats.bris.ac.uk/R/"
 # vector of packages to install/load
-packages <- c("dplyr","citr", "readr", "naniar", "gt", "gtsummary", "patchwork", "ggplot2", "ggfortify", "knitr", "gtExtras", "caret")
+packages <- c("dplyr","tidyr","citr", "readr", "naniar", "gt", "gtsummary", "patchwork", "ggplot2", "ggfortify", "knitr", "gtExtras", "corrplot")
 
 for (package in packages) {
   if (!require(package, character.only = TRUE)) {
