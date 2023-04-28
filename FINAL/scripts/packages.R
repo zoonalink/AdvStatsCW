a2cde3f18ed612361384ecff67d397488e55cf8c
petter Rstudio <zoonalink@gmail.com>
2023-04-28 23:26:41 +0100

    friday work - data section

--- FINAL/scripts/packages.R
+++ FINAL/scripts/packages.R
@@ -1,7 +1,7 @@
 
 cran_repo <- "https://www.stats.bris.ac.uk/R/"
 # vector of packages to install/load
-packages <- c("dplyr","tidyr","citr", "readr", "naniar", "gt", "gtsummary", "patchwork", "ggplot2", "ggfortify", "knitr", "gtExtras", "corrplot")
+packages <- c("dplyr","tidyr","citr", "readr", "naniar", "flextable", "patchwork", "ggplot2", "GGally", "ggfortify", "knitr", "corrplot", "ade4", "factoextra")
 
 for (package in packages) {
   if (!require(package, character.only = TRUE)) {
