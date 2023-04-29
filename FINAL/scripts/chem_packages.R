9442d43a3897c8d68549629c29eac28a8faf66a5
petter Rstudio <zoonalink@gmail.com>
2023-04-29 23:10:23 +0100

    flextable tidy, repeatable, eda start

---
+++ FINAL/scripts/chem_packages.R
@@ -0,0 +1,44 @@
+# R script to load packages required by separate quarto files
+
+
+# default packages
+default_packages <- c("dplyr", "tidyr", "naniar", "flextable", "ggplot2", "GGally", "knitr", "corrplot", "ade4", "factoextra", "cowplot")
+
+# assign packages based on file name
+packages <- default_packages
+if (file == "dim_red") {
+  packages <- c("dplyr", "flextable")
+} else if (file == "EDA") {
+  packages <- c("dplyr", "flextable", "ggplot2", "GGally", "corrplot", "cowplot")
+} else if (file == "data") {
+  packages <- c("dplyr", "flextable", "naniar")
+} else if (file == "intro") {
+  packages <- c("flextable")
+}
+
+# install and load packages
+cran_repo <- "https://www.stats.bris.ac.uk/R/"
+for (package in packages) {
+  if (!require(package, character.only = TRUE)) {
+    if (package == "citr") {
+      devtools::install_github("crsh/citr")
+    } else {
+      install.packages(package, repos = cran_repo)
+    }
+    library(package, character.only = TRUE)
+  }
+}
+
+set_flextable_defaults(
+  font.family = "Arial",
+  font.size = 10, 
+  font.color = "#333333",
+  border.color = "gray", 
+  background.color = "#FAFAFA",
+  table.layout= "autofit",
+  theme_fun = theme_vanilla,
+  padding.top = 3, padding.bottom = 3,
+  padding.left = 4, padding.right = 4,
+  na_str = "<na>", 
+  digits = 2
+)
--- FINAL/scripts/packages.R
+++ FINAL/scripts/packages.R
@@ -1,7 +1,11 @@
 
 cran_repo <- "https://www.stats.bris.ac.uk/R/"
 # vector of packages to install/load
-packages <- c("dplyr","tidyr","citr", "readr", "naniar", "flextable", "patchwork", "ggplot2", "GGally", "ggfortify", "knitr", "corrplot", "ade4", "factoextra")
+packages <- c("dplyr","tidyr",  "naniar", "flextable",  "ggplot2", "GGally",  "knitr", "corrplot", "ade4", "factoextra", "cowplot")
+
+data <- c("dplyr", "flextable", "naniar")
+
+eda <- c("dplyr", "flextable", "ggplot2", "GGally",  "knitr", "corrplot", "cowplot")
 
 for (package in packages) {
   if (!require(package, character.only = TRUE)) {
@@ -13,3 +17,14 @@ for (package in packages) {
     library(package, character.only = TRUE)
   }
 }
+
+
+
+
+#packages used during exploration but not needed for final report
+
+#"citr",
+#"patchwork",
+#"ggfortify",
+#"readr",
+#"knitr", 
