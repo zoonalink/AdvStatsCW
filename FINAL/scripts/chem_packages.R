5c15b37e8c9e1f822d094f24b2ef576a1059ed4f
petter Rstudio <zoonalink@gmail.com>
2023-05-01 14:33:10 +0100

    midday eda progress

--- FINAL/scripts/chem_packages.R
+++ FINAL/scripts/chem_packages.R
@@ -4,14 +4,14 @@
 set.seed(567)
 
 # default packages
-default_packages <- c("dplyr", "tidyr", "naniar", "flextable", "ggplot2", "GGally", "knitr", "corrplot", "ade4", "factoextra", "cowplot")
+default_packages <- c("dplyr", "tidyr", "naniar", "flextable", "ggplot2", "GGally", "knitr", "corrplot", "ade4", "factoextra", "cowplot", "patchwork")
 
 # assign packages based on file name
 packages <- default_packages
 if (file == "dim_red") {
   packages <- c("dplyr", "flextable")
 } else if (file == "EDA") {
-  packages <- c("dplyr", "flextable", "ggplot2", "GGally", "corrplot", "cowplot", "psych")
+  packages <- c("dplyr", "flextable", "ggplot2", "GGally", "corrplot", "cowplot", "psych", "patchwork")
 } else if (file == "data") {
   packages <- c("dplyr", "flextable", "naniar")
 } else if (file == "intro") {
@@ -31,20 +31,36 @@ for (package in packages) {
   }
 }
 
+# set_flextable_defaults(
+#   font.family = "Arial",
+#   font.size = 10, 
+#   font.color = "#616161",
+#   border.color = "gray", 
+#   background.color = "#C2D2CA",
+#   table.layout= "autofit",
+#   theme_fun = theme_vanilla,
+#   padding.top = 3, padding.bottom = 3,
+#   padding.left = 4, padding.right = 4,
+#   na_str = "<na>", 
+#   digits = 2
+# )
+
 set_flextable_defaults(
-  font.family = "Arial",
-  font.size = 10, 
-  font.color = "#616161",
-  border.color = "gray", 
-  background.color = "#A3C4BC",
-  table.layout= "autofit",
+  font.family = "Helvetica",
+  font.size = 12, 
+  font.color = "#333333",
+  border.color = "navy",
+  background.color = "white",
+  table.layout = "fixed",
   theme_fun = theme_vanilla,
-  padding.top = 3, padding.bottom = 3,
-  padding.left = 4, padding.right = 4,
-  na_str = "<na>", 
-  digits = 2
+  padding.top = 5, padding.bottom = 5,
+  padding.left = 6, padding.right = 6,
+  na_str = "NA", 
+  digits = 3
 )
+
 
 #font.color = "#333333",
 #border.color = "gray", 
 #background.color = "#FAFAFA",
+# A3C4BC colour in plots
